ae474d23fd62a9853520228f64eb6860408b36fe
Ekateryna Volkova <volkova5005@gmail.com>
2023-04-30 20:34:30 +0300

    fix: fix keyboard layout

--- script.js
+++ script.js
@@ -1,23 +1,400 @@
 class VirtualKeyboard {
   constructor(){
-    this.keys = { "_192":"~", "_49":"1", "_50":"2", "_51":"3", "_52":"4", "_53":"5", "_54":"6", "_55":"7", "_56":"8", "_57":"9", "_48":"0", "_189":"-", "_187":"+", "_8":"backspace",
-    "_9":"tab", "_81":"q", "_87":"w", "_69":"e", "_82":"r", "_84":"t", "_89":"y", "_85":"u", "_73":"i", "_79":"o", "_80":"p", "_219":"[", "_221":"]", "_220":"\\",
-    "_20": "caps", "_65":"a", "_83":"s", "_68":"d", "_70":"f", "_71":"g", "_72":"h", "_74":"j", "_75":"k", "_76":"l", "_186":";", "_222":"'", "_13":"enter",
-    "_16":"shift", "_90":"z", "_88":"x", "_67":"c", "_86":"v", "_66":"b", "_78":"n", "_77":"m", "_188":",", "_190":".", "_191":"?", "_16":"shift",
-    "_17":"ctrl", "_91":"Win", "_18":"alt", "_32":"space", "_18":"alt", "_92":"Win", "_17":"ctrl"};
+    this.keys = [
+      {
+        "code": 192,
+        "en": "~",
+        "ru": "~",
+        "class": "key"
+      },
+      {
+        "code": 49,
+        "en": 1,
+        "ru": 1,
+        "class": "key"
+      },
+      {
+        "code": 50,
+        "en": 2,
+        "ru": 2,
+        "class": "key"
+      },
+      {
+        "code": 51,
+        "en": 3,
+        "ru": 3,
+        "class": "key"
+      },
+      {
+        "code": 52,
+        "en": 4,
+        "ru": 4,
+        "class": "key"
+      },
+      {
+        "code": 53,
+        "en": 5,
+        "ru": 5,
+        "class": "key"
+      },
+      {
+        "code": 54,
+        "en": 6,
+        "ru": 6,
+        "class": "key"
+      },
+      {
+        "code": 55,
+        "en": 7,
+        "ru": 7,
+        "class": "key"
+      },
+      {
+        "code": 56,
+        "en": 8,
+        "ru": 8,
+        "class": "key"
+      },
+      {
+        "code": 57,
+        "en": 9,
+        "ru": 9,
+        "class": "key"
+      },
+      {
+        "code": 48,
+        "en": 0,
+        "ru": 0,
+        "class": "key"
+      },
+      {
+        "code": 189,
+        "en": "-",
+        "ru": "-",
+        "class": "key"
+      },
+      {
+        "code": 187,
+        "en": "=",
+        "ru": "=",
+        "class": "key"
+      },
+      {
+        "code": 8,
+        "en": "backspace",
+        "ru": "backspace",
+        "class": "key"
+      },
+      {
+        "code": 9,
+        "en": "tab",
+        "ru": "tab",
+        "class": "key"
+      },
+      {
+        "code": 81,
+        "en": "q",
+        "ru": "й",
+        "class": "key"
+      },
+      {
+        "code": 87,
+        "en": "w",
+        "ru": "ц",
+        "class": "key"
+      },
+      {
+        "code": 69,
+        "en": "e",
+        "ru": "у",
+        "class": "key"
+      },
+      {
+        "code": 82,
+        "en": "r",
+        "ru": "к",
+        "class": "key"
+      },
+      {
+        "code": 84,
+        "en": "t",
+        "ru": "е",
+        "class": "key"
+      },
+      {
+        "code": 89,
+        "en": "y",
+        "ru": "н",
+        "class": "key"
+      },
+      {
+        "code": 85,
+        "en": "u",
+        "ru": "г",
+        "class": "key"
+      },
+      {
+        "code": 73,
+        "en": "i",
+        "ru": "ш",
+        "class": "key"
+      },
+      {
+        "code": 79,
+        "en": "o",
+        "ru": "щ",
+        "class": "key"
+      },
+      {
+        "code": 80,
+        "en": "p",
+        "ru": "з",
+        "class": "key"
+      },
+      {
+        "code": 219,
+        "en": "[",
+        "ru": "х",
+        "class": "key"
+      },
+      {
+        "code": 221,
+        "en": "]",
+        "ru": "ъ",
+        "class": "key"
+      },
+      {
+        "code": 220,
+        "en": "\\",
+        "ru": "\\",
+        "class": "key"
+      },
+      {
+        "code": 20,
+        "en": "caps",
+        "ru": "caps",
+        "class": "key"
+      },
+      {
+        "code": 65,
+        "en": "a",
+        "ru": "ф",
+        "class": "key"
+      },
+      {
+        "code": 83,
+        "en": "s",
+        "ru": "ы",
+        "class": "key"
+      },
+      {
+        "code": 68,
+        "en": "d",
+        "ru": "в",
+        "class": "key"
+      },
+      {
+        "code": 70,
+        "en": "f",
+        "ru": "а",
+        "class": "key"
+      },
+      {
+        "code": 71,
+        "en": "g",
+        "ru": "п",
+        "class": "key"
+      },
+      {
+        "code": 72,
+        "en": "h",
+        "ru": "р",
+        "class": "key"
+      },
+      {
+        "code": 74,
+        "en": "j",
+        "ru": "о",
+        "class": "key"
+      },
+      {
+        "code": 75,
+        "en": "k",
+        "ru": "л",
+        "class": "key"
+      },
+      {
+        "code": 76,
+        "en": "l",
+        "ru": "д",
+        "class": "key"
+      },
+      {
+        "code": 186,
+        "en": ";",
+        "ru": "ж",
+        "class": "key"
+      },
+      {
+        "code": 222,
+        "en": "'",
+        "ru": "э",
+        "class": "key"
+      },
+      {
+        "code": 13,
+        "en": "enter",
+        "ru": "enter",
+        "class": "key key-enter"
+      },
+      {
+        "code": 16,
+        "en": "shift",
+        "ru": "shift",
+        "class": "key key-shift l-shift"
+      },
+      {
+        "code": 90,
+        "en": "z",
+        "ru": "я",
+        "class": "key"
+      },
+      {
+        "code": 88,
+        "en": "x",
+        "ru": "ч",
+        "class": "key"
+      },
+      {
+        "code": 67,
+        "en": "c",
+        "ru": "с",
+        "class": "key"
+      },
+      {
+        "code": 86,
+        "en": "v",
+        "ru": "м",
+        "class": "key"
+      },
+      {
+        "code": 66,
+        "en": "b",
+        "ru": "и",
+        "class": "key"
+      },
+      {
+        "code": 78,
+        "en": "n",
+        "ru": "т",
+        "class": "key"
+      },
+      {
+        "code": 77,
+        "en": "m",
+        "ru": "ь",
+        "class": "key"
+      },
+      {
+        "code": 188,
+        "en": ",",
+        "ru": "ю",
+        "class": "key"
+      },
+      {
+        "code": 190,
+        "en": ".",
+        "ru": "ю",
+        "class": "key"
+      },
+      {
+        "code": 191,
+        "en": "/",
+        "ru": ".",
+        "class": "key"
+      },
+      {
+        "code": 38,
+        "en": "▲",
+        "ru": "▲",
+        "class": "key key-up"
+      },
+      {
+        "code": 16,
+        "en": "shift",
+        "ru": "shift",
+        "class": "key key-shift"
+      },
+      {
+        "code": 17,
+        "en": "ctrl",
+        "ru": "ctrl",
+        "class": "key"
+      },
+      {
+        "code": 91,
+        "en": "win",
+        "ru": "win",
+        "class": "key"
+      },
+      {
+        "code": 18,
+        "en": "alt",
+        "ru": "alt",
+        "class": "key"
+      },
+      {
+        "code": 32,
+        "en": "space",
+        "ru": "space",
+        "class": "key key-space"
+      },
+      {
+        "code": 18,
+        "en": "alt",
+        "ru": "alt",
+        "class": "key"
+      },
+      {
+        "code": 17,
+        "en": "ctrl",
+        "ru": "ctrl",
+        "class": "key"
+      },
+      {
+        "code": 37,
+        "en": "◄",
+        "ru": "◄",
+        "class": "key"
+      },
+      {
+        "code": 40,
+        "en": "▼",
+        "ru": "▼",
+        "class": "key"
+      },
+      {
+        "code": 39,
+        "en": "►",
+        "ru": "►",
+        "class": "key"
+      },
+    ];
   }
 
-  renderKeyboard(){
+  renderKeyboard(lang='en'){
     const keys = this.keys;
     const body = document.querySelector("body");
-    let keyboard = "<textarea id='text-input' rows='4' cols='50'></textarea>";
+    let keyboard = "<div class='wrap'><h1 class='header'>RSS Виртуальная клавиатура</h1>";
+    keyboard += "<textarea id='text-input' rows='20' cols='50'></textarea>";
     keyboard += "<div class='keyboard'>";
-    for (let [key, value] in keys) {
-        console.log(key, value);
-      }
-    for (const key in keys) {
-        keyboard += `<span data-code="${key.slice(1)}" class='key'>${keys[key]}</span>`;
-    };
+    keys.forEach(keyObject => {
+      let value = keyObject[lang];
+      keyboard += `<span data-code="${keyObject['code']}" class="${keyObject['class']}">${value}</span>`;
+    });
+    keyboard += "</div>";
+    keyboard += "<p class='info'>Клавиатура создана в операционной системе Linux</p>";
+    keyboard += "<p class='info'>Для переключения языка комбинация: Win + space</p>"
     keyboard += "</div>";
     body.innerHTML = keyboard;
   }
